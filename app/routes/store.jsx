763b4aaab814a330bf9054168fba364fb505434c
Luis Gonzalez <lmgl887@gmail.com>
2022-12-26 01:36:13 -0600

    apply style to product list

--- app/routes/store.jsx
+++ app/routes/store.jsx
@@ -1,9 +1,27 @@
-//loader-> get data from third party
-//action -> get data from form
 import {getGuitars} from '~/models/guitars.server'
 import {useLoaderData} from '@remix-run/react'
 import Product from '~/components/product'
+import styles from '~/styles/products.css'
 
+
+export function meta(){
+  return{
+    title: 'GuitarSpot | Store',
+    description: 'Products collection'
+  }
+}
+
+export function links(){
+  return[
+    {
+      rel:'stylesheet',
+      href:styles
+    }
+  ]
+}
+
+//loader-> get data from third party
+//action -> get data from form
 export async function loader() {
   const guitars = await getGuitars()
   return guitars.data
